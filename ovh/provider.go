e208e00c24cd265a690119c43a26b5cd466cfbae
Radek Pieczonka <goblin@pentex.pl>
2018-05-17 10:06:00 +0200

    ovh_iploadbalancing_tcp_farm

--- ovh/provider.go
+++ ovh/provider.go
@@ -52,7 +52,8 @@ func Provider() terraform.ResourceProvider {
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
-			"ovh_domain_zone_record": resourceOvhDomainZoneRecord(),
+			"ovh_iploadbalancing_tcp_farm": resourceIpLoadbalancingTcpFarm(),
+			"ovh_domain_zone_record":       resourceOvhDomainZoneRecord(),
 			// New naming schema (issue #23)
 			"ovh_cloud_network_private":        resourcePublicCloudPrivateNetwork(),
 			"ovh_cloud_network_private_subnet": resourcePublicCloudPrivateNetworkSubnet(),
