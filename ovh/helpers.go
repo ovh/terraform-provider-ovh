8c5dfb44e7cbf34cddd4ccea754f6a35553d2214
Radek Pieczonka <goblin@pentex.pl>
2018-05-21 15:14:22 +0200

    adds iploadbalancing_tcp_farm_server

--- ovh/helpers.go
+++ ovh/helpers.go
@@ -1,6 +1,9 @@
 package ovh
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func validateStringEnum(value string, enum []string) error {
 	missing := true
@@ -13,4 +16,49 @@ func validateStringEnum(value string, enum []string) error {
 		return fmt.Errorf("Value %s is not among valid values (%s)", value, enum)
 	}
 	return nil
+}
+
+func getNilBoolPointer(val interface{}) *bool {
+	if val == nil {
+		return nil
+	}
+	value := val.(bool)
+	return &value
+}
+
+func getNilStringPointer(val interface{}) *string {
+	if val == nil {
+		return nil
+	}
+	value := val.(string)
+	if len(value) == 0 {
+		return nil
+	}
+	return &value
+}
+
+func getNilIntPointer(val interface{}) *int {
+	if val == nil {
+		return nil
+	}
+	value := val.(int)
+	return &value
+}
+
+func conditionalAttributeInt(buff *bytes.Buffer, name string, val *int) {
+	if val != nil {
+		buff.WriteString(fmt.Sprintf("  %s = %d\n", name, *val))
+	}
+}
+
+func conditionalAttributeString(buff *bytes.Buffer, name string, val *string) {
+	if val != nil {
+		buff.WriteString(fmt.Sprintf("  %s = \"%s\"\n", name, *val))
+	}
+}
+
+func conditionalAttributeBool(buff *bytes.Buffer, name string, val *bool) {
+	if val != nil {
+		buff.WriteString(fmt.Sprintf("  %s = %v\n", name, *val))
+	}
 }
