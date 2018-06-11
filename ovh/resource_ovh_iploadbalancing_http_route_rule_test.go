40929258d6616cb0493e261dac29a4a9f22ff2d6
Yann DEGAT <yann.degat@gmail.com>
2018-06-11 13:34:53 +0200

    Fix iplb http route acc tests

--- ovh/resource_ovh_iploadbalancing_http_route_rule_test.go
+++ ovh/resource_ovh_iploadbalancing_http_route_rule_test.go
@@ -1,192 +1,114 @@
 package ovh
 
 import (
-	"bytes"
 	"fmt"
 	"os"
-	"reflect"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 )
 
-var TestAccIPLoadbalancingRouteHTTPRulePlan = [][]map[string]interface{}{
-	{
-		{"DisplayName": "Test rule", "Field": "header", "Match": "is", "Negate": false, "Pattern": "example.com", "SubField": "Host"},
-	},
+func TestAccIPLoadbalancingRouteHTTPRuleBasicCreate(t *testing.T) {
+	serviceName := os.Getenv("OVH_IPLB_SERVICE")
+	displayName := "Test rule"
+	field := "header"
+	match := "is"
+	negate := "false"
+	pattern := "example.com"
+	subField := "Host"
+
+	config := fmt.Sprintf(
+		testAccCheckOvhIpLoadbalancingHttpRouteRuleConfig_basic,
+		serviceName,
+		displayName,
+		field,
+		match,
+		negate,
+		pattern,
+		subField,
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccCheckIpLoadbalancingRouteHTTPRulePreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIPLoadbalancingRouteHTTPRuleDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route.testroute", "service_name", serviceName),
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route.testroute", "display_name", displayName),
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route_rule.testrule", "service_name", serviceName),
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route_rule.testrule", "display_name", displayName),
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route_rule.testrule", "field", field),
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route_rule.testrule", "match", match),
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route_rule.testrule", "negate", negate),
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route_rule.testrule", "pattern", pattern),
+					resource.TestCheckResourceAttr(
+						"ovh_iploadbalancing_http_route_rule.testrule", "sub_field", subField),
+				),
+			},
+		},
+	})
 }
 
-type TestAccIPLoadbalancingRouteHTTPRule struct {
-	ServiceName string
-	RuleID      int    `json:"ruleId"`
-	RouteID     int    `json:"routeId"`
-	DisplayName string `json:"displayName"`
-	Field       string `json:"field"`
-	Match       string `json:"match"`
-	Negate      bool   `json:"negate"`
-	Pattern     string `json:"pattern"`
-	SubField    string `json:"subField"`
+func testAccCheckIpLoadbalancingRouteHTTPRulePreCheck(t *testing.T) {
+	testAccPreCheck(t)
+	testAccCheckIpLoadbalancingExists(t)
 }
 
-type TestAccIPLoadbalancingRouteHTTPRuleWrapper struct {
-	Expected *TestAccIPLoadbalancingRouteHTTPRule
-}
-
-func (w *TestAccIPLoadbalancingRouteHTTPRuleWrapper) Config() string {
-	var config bytes.Buffer
-	config.WriteString(fmt.Sprintf(`
-		resource "ovh_iploadbalancing_http_route" "testroute" {
-			service_name = "%s"
-			display_name = "Test route"
-			weight = 0
-
-			action {
-				status = 302
-				target = "http://example.com"
-				type = "redirect"
-			}
-		}
-
-		resource "ovh_iploadbalancing_http_route_rule" "testrule" {
-			service_name = "%s"
-			route_id  = "${ovh_iploadbalancing_http_route.testroute.id}"
-			display_name = "%s"
-			field = "%s"
-			match = "%s"
-			negate = %t
-			pattern = "%s"
-			sub_field = "%s"
-		}
-	`, w.Expected.ServiceName,
-		w.Expected.ServiceName,
-		w.Expected.DisplayName,
-		w.Expected.Field,
-		w.Expected.Match,
-		w.Expected.Negate,
-		w.Expected.Pattern,
-		w.Expected.SubField))
-
-	return config.String()
-}
-
-func (rule *TestAccIPLoadbalancingRouteHTTPRule) MustEqual(compared *TestAccIPLoadbalancingRouteHTTPRule) error {
-	if !reflect.DeepEqual(rule.DisplayName, compared.DisplayName) {
-		return fmt.Errorf("DisplayName differs")
-	}
-	if !reflect.DeepEqual(rule.Field, compared.Field) {
-		return fmt.Errorf("Field differs")
-	}
-	if !reflect.DeepEqual(rule.Match, compared.Match) {
-		return fmt.Errorf("Match differs")
-	}
-	if !reflect.DeepEqual(rule.Negate, compared.Negate) {
-		return fmt.Errorf("Negate differs")
-	}
-	if !reflect.DeepEqual(rule.Pattern, compared.Pattern) {
-		return fmt.Errorf("Pattern differs")
-	}
-	if !reflect.DeepEqual(rule.SubField, compared.SubField) {
-		return fmt.Errorf("SubField differs")
-	}
-
-	return nil
-}
-
-type TestAccIPLoadbalancingRouteHTTPRuleStep struct {
-	Response *TestAccIPLoadbalancingRouteHTTPRule
-	Expected *TestAccIPLoadbalancingRouteHTTPRule
-}
-
-func (w *TestAccIPLoadbalancingRouteHTTPRuleWrapper) TestStep(c map[string]interface{}) resource.TestStep {
-	if val, ok := c["DisplayName"]; ok {
-		w.Expected.DisplayName = val.(string)
-	}
-	if val, ok := c["Field"]; ok {
-		w.Expected.Field = val.(string)
-	}
-	if val, ok := c["Match"]; ok {
-		w.Expected.Match = val.(string)
-	}
-	if val, ok := c["Negate"]; ok {
-		w.Expected.Negate = val.(bool)
-	}
-	if val, ok := c["Pattern"]; ok {
-		w.Expected.Pattern = val.(string)
-	}
-	if val, ok := c["SubField"]; ok {
-		w.Expected.SubField = val.(string)
-	}
-	expected := *w.Expected
-
-	return resource.TestStep{
-		Config: w.Config(),
-		Check: resource.ComposeTestCheckFunc(
-			w.TestCheck(expected),
-		),
-	}
-}
-
-func (w *TestAccIPLoadbalancingRouteHTTPRuleWrapper) TestCheck(expected TestAccIPLoadbalancingRouteHTTPRule) resource.TestCheckFunc {
-	return func(state *terraform.State) error {
-		response := &TestAccIPLoadbalancingRouteHTTPRule{}
-		name := "ovh_iploadbalancing_http_route_rule.testrule"
-		resource, ok := state.RootModule().Resources[name]
-		if !ok {
-			return fmt.Errorf("Not found: %s", name)
-		}
-		config := testAccProvider.Meta().(*Config)
-		endpoint := fmt.Sprintf("/ipLoadbalancing/%s/http/route/%s/rule/%s", os.Getenv("OVH_IPLB_SERVICE"), resource.Primary.Attributes["route_id"], resource.Primary.ID)
-		err := config.OVHClient.Get(endpoint, response)
-		if err != nil {
-			return fmt.Errorf("calling GET %s :\n\t %s", endpoint, err.Error())
-		}
-
-		err = expected.MustEqual(response)
-		if err != nil {
-			return fmt.Errorf("%s %s state differs from expected : %s", name, resource.Primary.ID, err.Error())
-		}
-		return nil
-	}
-}
-
-func (w *TestAccIPLoadbalancingRouteHTTPRuleWrapper) TestDestroy(state *terraform.State) error {
-	leftovers := false
+func testAccCheckIPLoadbalancingRouteHTTPRuleDestroy(state *terraform.State) error {
 	for _, resource := range state.RootModule().Resources {
 		if resource.Type != "ovh_iploadbalancing_http_route_rule" {
 			continue
 		}
 
 		config := testAccProvider.Meta().(*Config)
-		endpoint := fmt.Sprintf("/ipLoadbalancing/%s/http/route/%d/rule/%s", os.Getenv("OVH_IPLB_SERVICE"), w.Expected.RouteID, resource.Primary.ID)
+		endpoint := fmt.Sprintf(
+			"/ipLoadbalancing/%s/http/route/%d/rule/%s",
+			os.Getenv("OVH_IPLB_SERVICE"),
+			resource.Primary.Attributes["route_id"],
+			resource.Primary.ID,
+		)
+
 		err := config.OVHClient.Get(endpoint, nil)
 		if err == nil {
-			leftovers = true
+			return fmt.Errorf("IpLoadbalancing http route rule still exists")
 		}
-	}
-	if leftovers {
-		return fmt.Errorf("IpLoadbalancing http route rule still exists")
 	}
 	return nil
 }
 
-func newTestAccIPLoadbalancingRouteHTTPRuleWrapper() *TestAccIPLoadbalancingRouteHTTPRuleWrapper {
-	return &TestAccIPLoadbalancingRouteHTTPRuleWrapper{
-		Expected: &TestAccIPLoadbalancingRouteHTTPRule{ServiceName: os.Getenv("OVH_IPLB_SERVICE")},
+const testAccCheckOvhIpLoadbalancingHttpRouteRuleConfig_basic = `
+resource "ovh_iploadbalancing_http_route" "testroute" {
+	service_name = "%s"
+	display_name = "%s"
+	weight = 0
+
+	action {
+		status = 302
+		target = "http://example.com"
+		type = "redirect"
 	}
 }
 
-func TestAccIpLoadbalancingRouteHTTPRuleBasicCreate(t *testing.T) {
-	for _, plan := range TestAccIPLoadbalancingRouteHTTPRulePlan {
-		w := newTestAccIPLoadbalancingRouteHTTPRuleWrapper()
-		var steps []resource.TestStep
-		for _, tcase := range plan {
-			steps = append(steps, w.TestStep(tcase))
-		}
-		resource.Test(t, resource.TestCase{
-			Providers:    testAccProviders,
-			CheckDestroy: w.TestDestroy,
-			Steps:        steps,
-		})
-	}
+resource "ovh_iploadbalancing_http_route_rule" "testrule" {
+	service_name = "${ovh_iploadbalancing_http_route.testroute.service_name}"
+	route_id  = "${ovh_iploadbalancing_http_route.testroute.id}"
+	display_name = "${ovh_iploadbalancing_http_route.testroute.display_name}"
+	field = "%s"
+	match = "%s"
+	negate = %s
+	pattern = "%s"
+	sub_field = "%s"
 }
+`
