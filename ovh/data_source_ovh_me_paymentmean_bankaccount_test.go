027cca707de9d9ed796cd3c64a73cfb66a58e3d1
Yann DEGAT <yann.degat@gmail.com>
2018-07-11 11:39:04 +0200

    Refactor paymentmeans to reflect convention
    - d/ovh_bank_account > d/ovh_me_paymentmean_bankaccount
    - d/ovh_credit_card > d/ovh_me_paymentmean_creditcard

---
+++ ovh/data_source_ovh_me_paymentmean_bankaccount_test.go
@@ -0,0 +1,36 @@
+package ovh
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccMePaymentmeanBankaccountDataSource_basic(t *testing.T) {
+	// ovh bank account payment mean is not mandatory
+	// this datasource is tested only if env var `OVH_TEST_BANKACCOUNT`
+	// is set to "1"
+	v := os.Getenv("OVH_TEST_BANKACCOUNT")
+	if v == "1" {
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { testAccPreCheck(t) },
+			Providers: testAccProviders,
+			Steps: []resource.TestStep{
+				resource.TestStep{
+					Config: testAccMePaymentmeanBankaccountDatasourceConfig,
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr(
+							"data.ovh_me_paymentmean_bankaccount.ba", "default", "true"),
+					),
+				},
+			},
+		})
+	}
+}
+
+const testAccMePaymentmeanBankaccountDatasourceConfig = `
+data "ovh_me_paymentmean_bankaccount" "ba" {
+ use_default = true
+}
+`
