b6e5b4f01aa34a786afb490054c3052d6378e481
Radek Pieczonka <goblin@pentex.pl>
2018-10-22 09:53:42 +0200

    wip

--- ovh/resource_ovh_dedicated_nasha_test.go
+++ ovh/resource_ovh_dedicated_nasha_test.go
@@ -3,43 +3,42 @@ package ovh
 import (
 	"bytes"
 	"fmt"
+	"net/url"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
 )
 
-// var TestAccDedicatedNASHAPlan = [][]map[string]interface{}{
-// 	{
-// 		{"Status": "active", "Address": "10.0.0.11", "Port": 80, "Weight": 3, "DisplayName": "testBackendA"},
-// 		{"Port": 8080, "Probe": true, "Backup": true},
-// 		{"Port": 8080, "Probe": false, "Backup": false, "Weight": 2, "DisplayName": "testBackendB"},
-// 	},
-// 	{
-// 		{"Status": "inactive", "Address": "10.0.0.12", "Port": 80},
-// 		{"Port": 8080, "ProxyProtocolVersion": "v2", "Ssl": true},
-// 		{"Port": 8080, "ProxyProtocolVersion": "v1", "Ssl": true, "Backup": false},
-// 		{"Port": 8080, "ProxyProtocolVersion": nil, "Ssl": true, "Backup": true, "Status": "active"},
-// 	},
-// }
+var TestAccDedicatedNASHAPlan = [][]map[string]interface{}{
+	{
+		{"Name": "testacc1", "Size": 10, "Protocol": "NFS", "IP": "10.10.10.10", "Mode": "Readonly"},
+	},
+	{
+		{"Name": "testacc2"},
+	},
+}
 
-// type TestAccDedicatedNASHA struct {
-// 	ServiceName          string
-// 	ServerId             int     `json:"serverId"`
-// 	BackendId            int     `json:"backendId"`
-// 	FarmId               int     `json:"farmId"`
-// 	DisplayName          *string `json:"displayName"`
-// 	Address              *string `json:"address"`
-// 	Cookie               *string `json:"cookie"`
-// 	Port                 *int    `json:"port"`
-// 	ProxyProtocolVersion *string `json:"proxyProtocolVersion"`
-// 	Chain                *string `json:"chain"`
-// 	Weight               *int    `json:"weight"`
-// 	Probe                *bool   `json:"probe"`
-// 	Ssl                  *bool   `json:"ssl"`
-// 	Backup               *bool   `json:"backup"`
-// 	Status               *string `json:"status"`
-// }
+type TestAccDedicatedNASHAPartition struct {
+	ServiceName string
+	Name        string
+	Protocol    string
+	Size        int
+}
 
-// type TestAccDedicatedNASHAWrapper struct {
-// 	Expected *TestAccDedicatedNASHA
-// }
+type TestAccDedicatedNASHAPartitionAccess struct {
+	ServiceName   string
+	PartitionName string
+	IP            string
+	Mode          *string `json:"displayName"`
+}
+
+type TestAccDedicatedNASHAWrapper struct {
+	ExpectedPartition       *TestAccDedicatedNASHAPartition
+	ExpectedPartitionAccess *TestAccDedicatedNASHAPartitionAccess
+}
 
 func (w *TestAccDedicatedNASHAWrapper) Config() string {
 	var config bytes.Buffer
@@ -47,188 +46,162 @@ func (w *TestAccDedicatedNASHAWrapper) Config() string {
     resource "ovh_dedicated_nasha_partition" "testacc" {
 	  service_name = "%s"
 	  name = "testacc"
+	  protocol = "NFS"
+	  size = 10
 	}
 
 	resource "ovh_dedicated_nasha_partition_access" "testacc" {
 	  service_name = "%s"
 	  partition_name = ""
-	  farm_id = "${ovh_iploadbalancing_tcp_farm.testacc.id}"
-	  address = "%s"
-	  status = "%s"
-	`, w.Expected.ServiceName,
-		w.Expected.ServiceName,
-		*w.Expected.Address,
-		*w.Expected.Status))
-	conditionalAttributeString(&config, "display_name", w.Expected.DisplayName)
-	conditionalAttributeInt(&config, "port", w.Expected.Port)
-	conditionalAttributeString(&config, "proxy_protocol_version", w.Expected.ProxyProtocolVersion)
-	conditionalAttributeInt(&config, "weight", w.Expected.Weight)
-	conditionalAttributeBool(&config, "probe", w.Expected.Probe)
-	conditionalAttributeBool(&config, "ssl", w.Expected.Ssl)
-	conditionalAttributeBool(&config, "backup", w.Expected.Backup)
+	  ip = ""`, w.ExpectedPartition.ServiceName, w.ExpectedPartitionAccess.PartitionName))
+	conditionalAttributeString(&config, "mode", w.ExpectedPartitionAccess.Mode)
 	config.WriteString(`}`)
 	return config.String()
 }
 
-// func (server *TestAccDedicatedNASHA) MustEqual(compared *TestAccDedicatedNASHA) error {
-// 	if !reflect.DeepEqual(server.DisplayName, compared.DisplayName) {
-// 		return fmt.Errorf("DisplayName differs")
-// 	}
-// 	if !reflect.DeepEqual(server.Address, compared.Address) {
-// 		return fmt.Errorf("Address differs")
-// 	}
-// 	if !reflect.DeepEqual(server.Port, compared.Port) {
-// 		return fmt.Errorf("Port differs")
-// 	}
-// 	if !reflect.DeepEqual(server.ProxyProtocolVersion, compared.ProxyProtocolVersion) {
-// 		return fmt.Errorf("ProxyProtocolVersion differs")
-// 	}
-// 	if !reflect.DeepEqual(server.Chain, compared.Chain) {
-// 		return fmt.Errorf("Chain differs")
-// 	}
-// 	if !reflect.DeepEqual(server.Weight, compared.Weight) {
-// 		return fmt.Errorf("Weight differs")
-// 	}
-// 	if !reflect.DeepEqual(server.Probe, compared.Probe) {
-// 		return fmt.Errorf("Probe differs")
-// 	}
-// 	if !reflect.DeepEqual(server.Ssl, compared.Ssl) {
-// 		return fmt.Errorf("Ssl differs")
-// 	}
-// 	if !reflect.DeepEqual(server.Backup, compared.Backup) {
-// 		return fmt.Errorf("Backup differs")
-// 	}
-// 	if !reflect.DeepEqual(server.Status, compared.Status) {
-// 		return fmt.Errorf("Status differs")
-// 	}
-// 	return nil
-// }
+func (original *TestAccDedicatedNASHAPartition) MustEqual(compared *TestAccDedicatedNASHAPartition) error {
+	if !reflect.DeepEqual(original.Name, compared.Name) {
+		return fmt.Errorf("Name differs")
+	}
+	return nil
+}
+
+func (original *TestAccDedicatedNASHAPartitionAccess) MustEqual(compared *TestAccDedicatedNASHAPartitionAccess) error {
+	if !reflect.DeepEqual(original.ServiceName, compared.ServiceName) {
+		return fmt.Errorf("ServiceName differs")
+	}
+	return nil
+}
 
 // type TestAccDedicatedNASHAStep struct {
 // 	Response *TestAccDedicatedNASHA
 // 	Expected *TestAccDedicatedNASHA
 // }
 
-// func (w *TestAccDedicatedNASHAWrapper) TestStep(c map[string]interface{}) resource.TestStep {
-// 	if val, ok := c["DisplayName"]; ok {
-// 		w.Expected.DisplayName = getNilStringPointer(val)
-// 	}
-// 	if val, ok := c["Address"]; ok {
-// 		w.Expected.Address = getNilStringPointer(val)
-// 	}
-// 	if val, ok := c["Port"]; ok {
-// 		w.Expected.Port = getNilIntPointer(val)
-// 	}
-// 	if val, ok := c["ProxyProtocolVersion"]; ok {
-// 		w.Expected.ProxyProtocolVersion = getNilStringPointer(val)
-// 	}
-// 	if val, ok := c["Chain"]; ok {
-// 		w.Expected.Chain = getNilStringPointer(val)
-// 	}
-// 	if val, ok := c["Weight"]; ok {
-// 		w.Expected.Weight = getNilIntPointer(val)
-// 	}
-// 	if val, ok := c["Probe"]; ok {
-// 		w.Expected.Probe = getNilBoolPointer(val)
-// 	}
-// 	if val, ok := c["Ssl"]; ok {
-// 		w.Expected.Ssl = getNilBoolPointer(val)
-// 	}
-// 	if val, ok := c["Backup"]; ok {
-// 		w.Expected.Backup = getNilBoolPointer(val)
-// 	}
-// 	if val, ok := c["Status"]; ok {
-// 		w.Expected.Status = getNilStringPointer(val)
-// 	}
+// Returns assembled terraform TestStep from test wrapper
+func (w *TestAccDedicatedNASHAWrapper) TestStep(c map[string]interface{}) resource.TestStep {
+	// if val, ok := c["Name"]; ok {
+	// 	w.ExpectedPartition.Name = getNilStringPointer(val)
+	// }
+	// if val, ok := c["Address"]; ok {
+	// 	w.ExpectedPartition.Address = getNilStringPointer(val)
+	// }
+	// if val, ok := c["Port"]; ok {
+	// 	w.ExpectedPartitionAccess.IP = getNilIntPointer(val)
+	// }
 
-// 	expected := *w.Expected
+	// expected := *w.Expected
 
-// 	// set OVH API defaults instead of nil before checking
-// 	if expected.Probe == nil {
-// 		val := false
-// 		expected.Probe = &val
-// 	}
-// 	if expected.Ssl == nil {
-// 		val := false
-// 		expected.Ssl = &val
-// 	}
-// 	if expected.Backup == nil {
-// 		val := false
-// 		expected.Backup = &val
-// 	}
-// 	if expected.Weight == nil {
-// 		val := 1
-// 		expected.Weight = &val
-// 	}
+	// set OVH API defaults instead of nil before checking
+	// if expected.Probe == nil {
+	// 	val := false
+	// 	expected.Probe = &val
+	// }
+	// if expected.Ssl == nil {
+	// 	val := false
+	// 	expected.Ssl = &val
+	// }
+	// if expected.Backup == nil {
+	// 	val := false
+	// 	expected.Backup = &val
+	// }
+	// if expected.Weight == nil {
+	// 	val := 1
+	// 	expected.Weight = &val
+	// }
 
-// 	return resource.TestStep{
-// 		Config: w.Config(),
-// 		Check: resource.ComposeTestCheckFunc(
-// 			w.TestCheck(expected),
-// 		),
-// 	}
-// }
+	return resource.TestStep{
+		Config: w.Config(),
+		Check: resource.ComposeTestCheckFunc(
+			w.TestCheck(w.ExpectedPartition, w.ExpectedPartitionAccess),
+		),
+	}
+}
 
-// func (w *TestAccDedicatedNASHAWrapper) TestCheck(expected TestAccDedicatedNASHA) resource.TestCheckFunc {
-// 	return func(state *terraform.State) error {
-// 		response := &TestAccDedicatedNASHA{}
-// 		name := "ovh_iploadbalancing_tcp_farm_server.testacc"
-// 		resource, ok := state.RootModule().Resources[name]
-// 		if !ok {
-// 			return fmt.Errorf("Not found: %s", name)
-// 		}
-// 		config := testAccProvider.Meta().(*Config)
-// 		endpoint := fmt.Sprintf("/ipLoadbalancing/%s/tcp/farm/%s/server/%s", os.Getenv("OVH_IPLB_SERVICE"), resource.Primary.Attributes["farm_id"], resource.Primary.ID)
-// 		err := config.OVHClient.Get(endpoint, response)
-// 		if err != nil {
-// 			return fmt.Errorf("calling GET %s :\n\t %s", endpoint, err.Error())
-// 		}
+func (w *TestAccDedicatedNASHAWrapper) TestCheck(expectedPartition *TestAccDedicatedNASHAPartition, expectedPartitionAccess *TestAccDedicatedNASHAPartitionAccess) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		// Check if resource states exists in local state at all
+		name := "ovh_dedicated_nasha_partition.testacc"
+		partitionResource, ok := state.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+		// Check if nasha partition state fetched from the server reflects the expected state
+		partitionResponse := &TestAccDedicatedNASHAPartition{}
+		config := testAccProvider.Meta().(*Config)
+		endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s", os.Getenv("OVH_NASHA_SERVICE"), partitionResource.Primary.Attributes["name"])
+		err := config.OVHClient.Get(endpoint, partitionResponse)
+		if err != nil {
+			return fmt.Errorf("calling GET %s :\n\t %s", endpoint, err.Error())
+		}
+		err = expectedPartition.MustEqual(partitionResponse)
+		if err != nil {
+			return fmt.Errorf("%s %s state differs from expected : %s", name, partitionResource.Primary.Attributes["name"], err.Error())
+		}
 
-// 		err = expected.MustEqual(response)
-// 		if err != nil {
-// 			return fmt.Errorf("%s %s state differs from expected : %s", name, resource.Primary.ID, err.Error())
-// 		}
-// 		return nil
-// 	}
-// }
+		// Check if resource states exists in local state at all
+		name = "ovh_dedicated_nasha_partition_access.testacc"
+		partitionAccessResource, ok := state.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("Not found: %s", name)
+		}
+		// Check if nasha_partition_access state fetched from the server reflects the expected state
+		partitionAccessResponse := &TestAccDedicatedNASHAPartitionAccess{}
+		config = testAccProvider.Meta().(*Config)
+		endpoint = fmt.Sprintf("/dedicated/nasha/%s/partition/%s", os.Getenv("OVH_NASHA_SERVICE"), partitionAccessResource.Primary.Attributes["name"])
+		err = config.OVHClient.Get(endpoint, partitionResponse)
+		if err != nil {
+			return fmt.Errorf("calling GET %s :\n\t %s", endpoint, err.Error())
+		}
+		err = expectedPartitionAccess.MustEqual(partitionAccessResponse)
+		if err != nil {
+			return fmt.Errorf("%s %s state differs from expected : %s", name, partitionAccessResource.Primary.Attributes["name"], err.Error())
+		}
 
-// func (w *TestAccDedicatedNASHAWrapper) TestDestroy(state *terraform.State) error {
-// 	leftovers := false
-// 	for _, resource := range state.RootModule().Resources {
-// 		if resource.Type != "ovh_iploadbalancing_tcp_farm_server" {
-// 			continue
-// 		}
+		return nil
+	}
+}
 
-// 		config := testAccProvider.Meta().(*Config)
-// 		endpoint := fmt.Sprintf("/ipLoadbalancing/%s/tcp/farm/%d/server/%s", os.Getenv("OVH_IPLB_SERVICE"), w.Expected.FarmId, resource.Primary.ID)
-// 		err := config.OVHClient.Get(endpoint, nil)
-// 		if err == nil {
-// 			leftovers = true
-// 		}
-// 	}
-// 	if leftovers {
-// 		return fmt.Errorf("IpLoadbalancing farm still exists")
-// 	}
-// 	return nil
-// }
+func (w *TestAccDedicatedNASHAWrapper) TestDestroy(state *terraform.State) error {
+	leftovers := false
+	// Removing partition will also remove all its defined access rules
+	for _, resource := range state.RootModule().Resources {
+		if resource.Type == "ovh_dedicated_nasha_partition" {
+			config := testAccProvider.Meta().(*Config)
+			service := os.Getenv("OVH_NASHA_SERVICE")
+			partition := w.ExpectedPartition.Name
+			ip := url.PathEscape(resource.Primary.Attributes["ip"])
+			endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s/access/%s", service, partition, ip)
+			err := config.OVHClient.Get(endpoint, nil)
+			if err == nil {
+				leftovers = true
+			}
+		}
+	}
+	if leftovers {
+		return fmt.Errorf("ovh_dedicated_nasha_partition still exists")
+	}
+	return nil
+}
 
-// func newTestAccDedicatedNASHAWrapper() *TestAccDedicatedNASHAWrapper {
-// 	return &TestAccDedicatedNASHAWrapper{
-// 		Expected: &TestAccDedicatedNASHA{ServiceName: os.Getenv("OVH_IPLB_SERVICE")},
-// 	}
-// }
+func newTestAccDedicatedNASHAWrapper() *TestAccDedicatedNASHAWrapper {
+	return &TestAccDedicatedNASHAWrapper{
+		ExpectedPartition:       &TestAccDedicatedNASHAPartition{ServiceName: os.Getenv("OVH_NASHA_SERVICE")},
+		ExpectedPartitionAccess: &TestAccDedicatedNASHAPartitionAccess{ServiceName: os.Getenv("OVH_NASHA_SERVICE")},
+	}
+}
 
-// func TestAccDedicatedNASHABasicCreate(t *testing.T) {
-// 	for _, plan := range TestAccDedicatedNASHAPlan {
-// 		w := newTestAccDedicatedNASHAWrapper()
-// 		var steps []resource.TestStep
-// 		for _, tcase := range plan {
-// 			steps = append(steps, w.TestStep(tcase))
-// 		}
-// 		resource.Test(t, resource.TestCase{
-// 			Providers:    testAccProviders,
-// 			CheckDestroy: w.TestDestroy,
-// 			Steps:        steps,
-// 		})
-// 	}
-// }
+func TestAccDedicatedNASHABasicCreate(t *testing.T) {
+	for _, plan := range TestAccDedicatedNASHAPlan {
+		w := newTestAccDedicatedNASHAWrapper()
+		var steps []resource.TestStep
+		for _, tcase := range plan {
+			steps = append(steps, w.TestStep(tcase))
+		}
+		resource.Test(t, resource.TestCase{
+			Providers:    testAccProviders,
+			CheckDestroy: w.TestDestroy,
+			Steps:        steps,
+		})
+	}
+}
