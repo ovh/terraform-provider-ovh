fc5f066a354aca2ba07cf4e353b4ff06f22faf4b
yann degat <yann.degat@corp.ovh.com>
2019-11-14 16:19:06 +0000

    r/ip_reverse: add sweeper

--- ovh/resource_ovh_ip_reverse_test.go
+++ ovh/resource_ovh_ip_reverse_test.go
@@ -2,11 +2,16 @@ package ovh
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	"github.com/ovh/go-ovh/ovh"
 )
 
 var testAccIpReverseConfig = fmt.Sprintf(`
@@ -16,6 +21,48 @@ resource "ovh_ip_reverse" "reverse" {
     reverse = "%s"
 }
 `, os.Getenv("OVH_IP_BLOCK"), os.Getenv("OVH_IP"), os.Getenv("OVH_IP_REVERSE"))
+
+func init() {
+	resource.AddTestSweepers("ovh_me_installation_template", &resource.Sweeper{
+		Name: "ovh_ip_reverse",
+		F:    testSweepIpReverse,
+	})
+}
+
+func testSweepIpReverse(region string) error {
+	client, err := sharedClientForRegion(region)
+	if err != nil {
+		return fmt.Errorf("error getting client: %s", err)
+	}
+
+	reverse := OvhIpReverse{}
+	testIp := os.Getenv("OVH_IP_BLOCK")
+	testIpReverse := os.Getenv("OVH_IP")
+	endpoint := fmt.Sprintf("/ip/%s/reverse/%s", strings.Replace(testIp, "/", "%2F", 1), testIpReverse)
+	if err := client.Get(endpoint, &reverse); err != nil {
+		if err.(*ovh.APIError).Code == 404 {
+			// no ip reverse set, nothing to sweep
+			return nil
+		}
+
+		return fmt.Errorf("Error calling %s:\n\t %q", endpoint, err)
+	}
+
+	log.Printf("[DEBUG] ip reverse found %v", reverse)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		log.Printf("[INFO] Deleting reverse %v", reverse)
+		if err := client.Delete(endpoint, nil); err != nil {
+			return resource.RetryableError(err)
+		}
+		// Successful delete
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func TestAccIpReverse_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
