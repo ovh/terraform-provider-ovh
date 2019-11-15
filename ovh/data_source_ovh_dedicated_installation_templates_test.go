b9f567b5a9bbbbfe4b9657d275e92362c049a3ec
yann degat <yann.degat@corp.ovh.com>
2019-11-15 09:28:48 +0000

    new datasource: ovh_dedicated_installation_templates

---
+++ ovh/data_source_ovh_dedicated_installation_templates_test.go
@@ -0,0 +1,23 @@
+package ovh
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDedicatedInstallationTemplatesDataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheckCredentials(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: "data ovh_dedicated_installation_templates templates {}",
+				Check: resource.TestCheckResourceAttrSet(
+					"data.ovh_dedicated_installation_templates.templates",
+					"result.#",
+				),
+			},
+		},
+	})
+}
