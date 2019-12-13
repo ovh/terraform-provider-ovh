770c364315292317d783e554d3e3da0090f1f9c2
yann degat <yann.degat@corp.ovh.com>
2019-12-13 16:19:01 +0000

    feat/r/dedicated_server_install_task: new resource

--- ovh/resource_ovh_dedicated_server_install_task.go
+++ ovh/resource_ovh_dedicated_server_install_task.go
@@ -15,8 +15,13 @@ import (
 func resourceDedicatedServerInstallTask() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceDedicatedServerInstallTaskCreate,
+		Update: resourceDedicatedServerInstallTaskUpdate,
 		Read:   resourceDedicatedServerInstallTaskRead,
 		Delete: resourceDedicatedServerInstallTaskDelete,
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(45 * time.Minute),
+		},
 
 		Schema: map[string]*schema.Schema{
 			"service_name": {
@@ -25,13 +30,101 @@ func resourceDedicatedServerInstallTask() *schema.Resource {
 				ForceNew:    true,
 				Description: "The internal name of your dedicated server.",
 			},
-			"keepers": {
-				Type:        schema.TypeList,
+			"partition_scheme_name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+				Description: "Partition scheme name.",
+			},
+			"template_name": {
+				Type:        schema.TypeString,
 				Required:    true,
 				ForceNew:    true,
-				Description: "Change this value to recreate an install task.",
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
+				Description: "Template name",
+			},
+			"bootid_on_destroy": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "If set, reboot the server on the specified boot id during destroy phase",
+			},
+			"details": {
+				Type:     schema.TypeList,
+				Optional: true,
+				ForceNew: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"change_log": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Template change log details",
+						},
+						"custom_hostname": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Set up the server using the provided hostname instead of the default hostname",
+						},
+						"disk_group_id": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "",
+						},
+						"install_rtm": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "",
+						},
+						"install_sql_server": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "",
+						},
+						"language": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "language",
+						},
+						"no_raid": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "",
+						},
+						"post_installation_script_link": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Indicate the URL where your postinstall customisation script is located",
+						},
+						"post_installation_script_return": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "indicate the string returned by your postinstall customisation script on successful execution. Advice: your script should return a unique validation string in case of succes. A good example is 'loh1Xee7eo OK OK OK UGh8Ang1Gu'",
+						},
+						"reset_hw_raid": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "",
+						},
+						"soft_raid_devices": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "",
+						},
+						"ssh_key_name": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Name of the ssh key that should be installed. Password login will be disabled",
+						},
+						"use_spla": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "",
+						},
+						"use_distrib_kernel": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Use the distribution's native kernel instead of the recommended OVH Kernel",
+						},
+					},
 				},
 			},
 
@@ -75,13 +168,13 @@ func resourceDedicatedServerInstallTaskCreate(d *schema.ResourceData, meta inter
 	serviceName := d.Get("service_name").(string)
 
 	endpoint := fmt.Sprintf(
-		"/dedicated/server/%s/reboot",
+		"/dedicated/server/%s/install/start",
 		url.PathEscape(serviceName),
 	)
-
+	opts := (&DedicatedServerInstallTaskCreateOpts{}).FromResource(d)
 	task := &DedicatedServerTask{}
 
-	if err := config.OVHClient.Post(endpoint, nil, task); err != nil {
+	if err := config.OVHClient.Post(endpoint, opts, task); err != nil {
 		return fmt.Errorf("Error calling POST %s:\n\t %q", endpoint, err)
 	}
 
@@ -101,7 +194,7 @@ func resourceDedicatedServerInstallTaskRead(d *schema.ResourceData, meta interfa
 	id, err := strconv.ParseInt(d.Id(), 10, 64)
 	if err != nil {
 		return fmt.Errorf(
-			"Could not parse reboot task id %s,%s:\n\t %q",
+			"Could not parse install task id %s,%s:\n\t %q",
 			serviceName,
 			d.Id(),
 			err,
@@ -131,7 +224,32 @@ func resourceDedicatedServerInstallTaskRead(d *schema.ResourceData, meta interfa
 	return nil
 }
 
+func resourceDedicatedServerInstallTaskUpdate(d *schema.ResourceData, meta interface{}) error {
+	// nothing to do on update
+	return resourceDedicatedServerInstallTaskRead(d, meta)
+}
+
 func resourceDedicatedServerInstallTaskDelete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	bootId := getNilIntPointerFromData(d, "bootid_on_destroy")
+
+	if bootId != nil {
+		serviceName := d.Get("service_name").(string)
+		endpoint := fmt.Sprintf(
+			"/dedicated/server/%s/reboot",
+			url.PathEscape(serviceName),
+		)
+
+		task := &DedicatedServerTask{}
+		if err := config.OVHClient.Post(endpoint, nil, task); err != nil {
+			return fmt.Errorf("Error calling POST %s:\n\t %q", endpoint, err)
+		}
+
+		if err := waitForDedicatedServerTask(serviceName, task, config.OVHClient); err != nil {
+			return err
+		}
+	}
+
 	// we cant delete the task through the API, just forget about its Id
 	d.SetId("")
 	return nil
