9338c46649d4dd27a87a4808fe788dce1f4387ce
yann degat <yann.degat@corp.ovh.com>
2020-04-27 11:06:00 +0000

    feat: add me/ipxeScript datasources and resource

---
+++ ovh/data_source_ovh_me_ipxe_scripts.go
@@ -0,0 +1,44 @@
+package ovh
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/hashcode"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceMeIpxeScripts() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceMeIpxeScriptsRead,
+		Schema: map[string]*schema.Schema{
+			// Computed
+			"result": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+	}
+}
+
+// Common function with the datasource
+func dataSourceMeIpxeScriptsRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+
+	ids := []string{}
+	err := config.OVHClient.Get("/me/ipxeScript", &ids)
+
+	if err != nil {
+		return fmt.Errorf("Error calling /me/ipxeScript:\n\t %q", err)
+	}
+
+	// sort.Strings sorts in place, returns nothing
+	sort.Strings(ids)
+
+	d.SetId(hashcode.Strings(ids))
+	d.Set("result", ids)
+	return nil
+}
---
+++ website/docs/r/me_ipxe_script.html.markdown
@@ -0,0 +1,38 @@
+---
+layout: "ovh"
+page_title: "OVH: ovh_me_ipxe_script"
+sidebar_current: "docs-ovh-resource-me-ipxe-script"
+description: |-
+  Creates an IPXE Script.
+---
+
+# ovh_me_ipxe_script
+
+Creates an IPXE Script.
+
+## Example Usage
+
+```hcl
+resource "ovh_me_ipxe_script" "script" {
+  name   = "myscript"
+  script = file("${path.module}/boot.ipxe")
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `description` - For documentation purpose only. This attribute is not passed to the OVH API as it cannot be retrieved back. Instead a fake description ('$name auto description') is passed at creation time.
+
+* `name` - (Required) The name of the IPXE Script.
+
+* `script` - (Required) The content of the script.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `description` - See Argument Reference above.
+* `name` - See Argument Reference above.
+* `script` - See Argument Reference above.
