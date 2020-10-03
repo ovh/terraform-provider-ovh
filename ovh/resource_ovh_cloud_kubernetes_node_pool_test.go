260cb2cbeff01a26cc9bc1200c6d9b8f0cfc8cac
Philippe Vienne <philippegeek@gmail.com>
2020-10-03 14:53:11 +0200

    Remove kubernetes node objects

--- ovh/resource_ovh_cloud_kubernetes_node_pool_test.go
+++ ovh/resource_ovh_cloud_kubernetes_node_pool_test.go
@@ -9,20 +9,23 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-var testAccPublicCloudKubernetesNodeConfig = fmt.Sprintf(`
+var testAccPublicCloudKubernetesNodePoolConfig = fmt.Sprintf(`
 data "ovh_cloud_kubernetes_cluster" "cluster" {
 	project_id  = "%s"
 	name = "%s"
 }
-resource "ovh_cloud_kubernetes_node" "node" {
+resource "ovh_cloud_kubernetes_node_pool" "pool" {
 	project_id  = "%s"
 	cluster_id = data.ovh_cloud_kubernetes_cluster.cluster.id
   	name = "acceptance-node"
 	flavor = "b2-7"
+	desiredSize = 1
+	minSize = 0
+	maxSize = 1
 }
 `, os.Getenv("OVH_PUBLIC_CLOUD"), os.Getenv("OVH_KUBERNETES_CLUSTER_NAME"), os.Getenv("OVH_PUBLIC_CLOUD"))
 
-func TestAccPublicCloudKubernetesNode_basic(t *testing.T) {
+func TestAccPublicCloudKubernetesNodePool_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheckCloud(t)
@@ -30,19 +33,19 @@ func TestAccPublicCloudKubernetesNode_basic(t *testing.T) {
 			testAccPreCheckKubernetes(t)
 		},
 		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckPublicCloudKubernetesNodeDestroy,
+		CheckDestroy: testAccCheckPublicCloudKubernetesNodePoolDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccPublicCloudKubernetesNodeConfig,
+				Config: testAccPublicCloudKubernetesNodePoolConfig,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckPublicCloudKubernetesNodeExists("ovh_cloud_kubernetes_node.node", t),
+					testAccCheckPublicCloudKubernetesNodePoolExists("ovh_cloud_kubernetes_node_pool.pool", t),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckPublicCloudKubernetesNodeExists(n string, t *testing.T) resource.TestCheckFunc {
+func testAccCheckPublicCloudKubernetesNodePoolExists(n string, t *testing.T) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		config := testAccProvider.Meta().(*Config)
 
@@ -52,27 +55,27 @@ func testAccCheckPublicCloudKubernetesNodeExists(n string, t *testing.T) resourc
 		}
 
 		if rs.Primary.ID == "" {
-			return fmt.Errorf("No ID is set")
+			return fmt.Errorf("no ID is set")
 		}
 
 		if rs.Primary.Attributes["project_id"] == "" {
-			return fmt.Errorf("No Project ID is set")
+			return fmt.Errorf("no Project ID is set")
 		}
 
-		return cloudKubernetesNodeExists(rs.Primary.Attributes["project_id"], rs.Primary.Attributes["cluster_id"], rs.Primary.ID, config.OVHClient)
+		return cloudKubernetesNodePoolExists(rs.Primary.Attributes["project_id"], rs.Primary.Attributes["cluster_id"], rs.Primary.ID, config.OVHClient)
 	}
 }
 
-func testAccCheckPublicCloudKubernetesNodeDestroy(s *terraform.State) error {
+func testAccCheckPublicCloudKubernetesNodePoolDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*Config)
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "ovh_cloud_kubernetes_cluster" {
 			continue
 		}
 
-		err := cloudKubernetesNodeExists(rs.Primary.Attributes["project_id"], rs.Primary.Attributes["cluster_id"], rs.Primary.ID, config.OVHClient)
+		err := cloudKubernetesNodePoolExists(rs.Primary.Attributes["project_id"], rs.Primary.Attributes["cluster_id"], rs.Primary.ID, config.OVHClient)
 		if err == nil {
-			return fmt.Errorf("cloud > Kubernetes Node still exists")
+			return fmt.Errorf("cloud > Kubernetes Pool still exists")
 		}
 
 	}
