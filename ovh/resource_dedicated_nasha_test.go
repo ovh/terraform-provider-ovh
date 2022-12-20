ba4461b2277aeb05daee13d82b6b28031bfa521d
Florian Perrot <florian.perrot@atgp.net>
2022-12-20 12:09:25 +0100

    feat(nasha): resource testing

--- ovh/resource_dedicated_nasha_test.go
+++ ovh/resource_dedicated_nasha_test.go
@@ -1,241 +1,164 @@
 package ovh
 
 import (
-	"bytes"
-	"crypto/md5"
-	"encoding/hex"
 	"fmt"
 	"net/url"
 	"os"
-	"reflect"
 	"testing"
-	"time"
 
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/ovh/terraform-provider-ovh/ovh/helpers"
 )
 
-var TestAccDedicatedNASHAPlan = [][]map[string]interface{}{
-	{
-		{"Size": 10, "Protocol": "NFS", "IP": "149.202.78.2/32", "Type": "readonly"},
-		// {"Size": 20, "Protocol": "CIFS", "IP": "149.202.78.2/32", "Type": "readonly"},
-	},
+func TestAccDedicatedNASHA(t *testing.T) {
+	serviceName := os.Getenv("OVH_NASHA_SERVICE_TEST")
+	partitionName := acctest.RandomWithPrefix(test_prefix)
+
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck: func() {
+			testAccPreCheckCredentials(t)
+			checkEnvOrFail(t, "OVH_NASHA_SERVICE_TEST")
+		},
+		CheckDestroy: testAccNashaPartitionDestroy("ovh_dedicated_nasha_partition.testacc"),
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+					resource "ovh_dedicated_nasha_partition" "testacc" {
+						service_name = "%s"
+						name = "%s"
+						description = "test description"
+						protocol = "NFS"
+						size = 10
+					}
+
+					resource "ovh_dedicated_nasha_partition_snapshot" "testacc" {
+						service_name = "${ovh_dedicated_nasha_partition.testacc.service_name}"
+						partition_name = "${ovh_dedicated_nasha_partition.testacc.name}"
+						type = "day-3"
+					}
+
+					resource "ovh_dedicated_nasha_partition_access" "testacc" {
+						service_name = "${ovh_dedicated_nasha_partition.testacc.service_name}"
+						partition_name = "${ovh_dedicated_nasha_partition.testacc.name}"
+						ip = "127.0.0.1/32"
+						type = "readonly"
+					}
+				`, serviceName, partitionName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccNashaPartitionCheck("ovh_dedicated_nasha_partition.testacc"),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition.testacc", "name", partitionName),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition.testacc", "description", "test description"),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition.testacc", "protocol", "NFS"),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition.testacc", "size", "10"),
+
+					testAccNashaPartitionSnapshotCheck("ovh_dedicated_nasha_partition_snapshot.testacc"),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition_snapshot.testacc", "partition_name", partitionName),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition_snapshot.testacc", "type", "day-3"),
+
+					testAccNashaPartitionAccessCheck("ovh_dedicated_nasha_partition_access.testacc"),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition_access.testacc", "partition_name", partitionName),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition_access.testacc", "ip", "127.0.0.1/32"),
+					resource.TestCheckResourceAttr("ovh_dedicated_nasha_partition_access.testacc", "type", "readonly"),
+				),
+			},
+		},
+	})
 }
 
-type TestAccDedicatedNASHAPartition struct {
-	ServiceName     string
-	Name            string `json:"partitionName"`
-	Protocol        string `json:"protocol"`
-	Size            int    `json:"size"`
-	Capacity        int    `json:"partitionCapacity"`
-	UsedBySnapshots int    `json:"usedBySnapshots"`
-}
-
-type TestAccDedicatedNASHAPartitionAccess struct {
-	ServiceName   string  `json:"serviceName"`
-	PartitionName string  `json:"partitionName"`
-	IP            string  `json:"ip"`
-	Type          *string `json:"displayName"`
-}
-
-type TestAccDedicatedNASHAWrapper struct {
-	RandomName              string
-	ExpectedPartition       *TestAccDedicatedNASHAPartition
-	ExpectedPartitionAccess *TestAccDedicatedNASHAPartitionAccess
-}
-
-func (w *TestAccDedicatedNASHAWrapper) Config() string {
-	var config bytes.Buffer
-	config.WriteString(fmt.Sprintf(`
-	resource "ovh_dedicated_nasha_partition" "testacc" {
-		service_name = "%s"
-		name = "%s"
-		protocol = "%s"
-		size = %d
-	}
-	resource "ovh_dedicated_nasha_partition_access" "testacc" {
-		service_name = "${ovh_dedicated_nasha_partition.testacc.service_name}"
-		partition_name = "${ovh_dedicated_nasha_partition.testacc.name}"
-		ip = "%s"
-	`,
-		w.ExpectedPartition.ServiceName,
-		w.ExpectedPartition.Name,
-		w.ExpectedPartition.Protocol,
-		w.ExpectedPartition.Size,
-		w.ExpectedPartitionAccess.IP))
-	helpers.ConditionalAttributeString(&config, "type", w.ExpectedPartitionAccess.Type)
-	config.WriteString(`}`)
-	return config.String()
-}
-
-func (original *TestAccDedicatedNASHAPartition) MustEqual(compared *TestAccDedicatedNASHAPartition) error {
-	if original.Name != compared.Name {
-		return fmt.Errorf("Name differs ('%s' vs '%s')", original.Name, compared.Name)
-	}
-	if original.Protocol != compared.Protocol {
-		return fmt.Errorf("Protocol differs ('%s' vs '%s')", original.Protocol, compared.Protocol)
-	}
-	if original.Size != compared.Size {
-		return fmt.Errorf("Size differs ('%d' vs '%d')", original.Size, compared.Size)
-	}
-	if original.Capacity != compared.Capacity {
-		return fmt.Errorf("Capacity differs ('%d' vs '%d')", original.Capacity, compared.Capacity)
-	}
-	if original.UsedBySnapshots != compared.UsedBySnapshots {
-		return fmt.Errorf("UsedBySnapshots differs ('%d' vs '%d')", original.UsedBySnapshots, compared.UsedBySnapshots)
-	}
-	return nil
-}
-
-func (original *TestAccDedicatedNASHAPartitionAccess) MustEqual(compared *TestAccDedicatedNASHAPartitionAccess) error {
-	if original.ServiceName != compared.ServiceName {
-		return fmt.Errorf("ServiceName differs ('%s' vs '%s')", original.ServiceName, compared.ServiceName)
-	}
-	if original.PartitionName != compared.PartitionName {
-		return fmt.Errorf("PartitionName differs ('%s' vs '%s')", original.PartitionName, compared.PartitionName)
-	}
-	if original.IP != compared.IP {
-		return fmt.Errorf("ServiceName differs ('%s' vs '%s')", original.IP, compared.IP)
-	}
-	if !reflect.DeepEqual(original.Type, compared.Type) {
-		return fmt.Errorf("Type differs ('%s' vs '%s')", *original.Type, *compared.Type)
-	}
-	return nil
-}
-
-// Returns assembled terraform TestStep from test wrapper
-func (w *TestAccDedicatedNASHAWrapper) TestStep(c map[string]interface{}) resource.TestStep {
-	w.ExpectedPartition.ServiceName = os.Getenv("OVH_NASHA_SERVICE")
-	w.ExpectedPartitionAccess.ServiceName = os.Getenv("OVH_NASHA_SERVICE")
-
-	if val, ok := c["Protocol"]; ok {
-		w.ExpectedPartition.Protocol = val.(string)
-	}
-	if val, ok := c["Size"]; ok {
-		w.ExpectedPartition.Size = val.(int)
-	}
-	if val, ok := c["IP"]; ok {
-		w.ExpectedPartitionAccess.IP = val.(string)
-	}
-	if val, ok := c["Type"]; ok {
-		w.ExpectedPartitionAccess.Type = helpers.GetNilStringPointer(val)
-	}
-
-	// set OVH API defaults instead of nil before checking
-	if w.ExpectedPartitionAccess.Type == nil {
-		val := "readwrite"
-		w.ExpectedPartitionAccess.Type = &val
-	}
-
-	return resource.TestStep{
-		Config: w.Config(),
-		Check: resource.ComposeTestCheckFunc(
-			w.TestCheck(w.ExpectedPartition, w.ExpectedPartitionAccess),
-		),
-	}
-}
-
-func (w *TestAccDedicatedNASHAWrapper) TestCheck(expectedPartition *TestAccDedicatedNASHAPartition, expectedPartitionAccess *TestAccDedicatedNASHAPartitionAccess) resource.TestCheckFunc {
+func testAccNashaPartitionCheck(n string) resource.TestCheckFunc {
 	return func(state *terraform.State) error {
-		// Check if resource states exists in local state at all
-		name := "ovh_dedicated_nasha_partition.testacc"
-		partitionResource, ok := state.RootModule().Resources[name]
+		partitionResource, ok := state.RootModule().Resources[n]
 		if !ok {
-			return fmt.Errorf("Not found: %s", name)
+			return fmt.Errorf("Resource not found: %s", n)
 		}
-		// Check if nasha partition state fetched from the server reflects the expected state
-		partitionResponse := &TestAccDedicatedNASHAPartition{}
+
+		serviceName := partitionResource.Primary.Attributes["service_name"]
+		partitionName := partitionResource.Primary.Attributes["name"]
+
 		config := testAccProvider.Meta().(*Config)
-		endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s", os.Getenv("OVH_NASHA_SERVICE"), partitionResource.Primary.Attributes["name"])
+		endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s", serviceName, partitionName)
+		partitionResponse := &DedicatedNASHAPartition{}
 		err := config.OVHClient.Get(endpoint, partitionResponse)
 		if err != nil {
 			return fmt.Errorf("calling GET %s :\n\t %s", endpoint, err.Error())
 		}
-		err = expectedPartition.MustEqual(partitionResponse)
-		if err != nil {
-			return fmt.Errorf("%s %s state differs from expected : %s", name, partitionResource.Primary.Attributes["name"], err.Error())
-		}
 
-		// // Check if resource states exists in local state at all
-		// name = "ovh_dedicated_nasha_partition_access.testacc"
-		// partitionAccessResource, ok := state.RootModule().Resources[name]
-		// if !ok {
-		// 	return fmt.Errorf("Not found: %s", name)
-		// }
-		// // Check if nasha_partition_access state fetched from the server reflects the expected state
-		// partitionAccessResponse := &TestAccDedicatedNASHAPartitionAccess{}
-		// config = testAccProvider.Meta().(*Config)
-		// endpoint = fmt.Sprintf("/dedicated/nasha/%s/partition/%s", os.Getenv("OVH_NASHA_SERVICE"), partitionAccessResource.Primary.Attributes["name"])
-		// err = config.OVHClient.Get(endpoint, partitionResponse)
-		// if err != nil {
-		// 	return fmt.Errorf("calling GET %s :\n\t %s", endpoint, err.Error())
-		// }
-		// err = expectedPartitionAccess.MustEqual(partitionAccessResponse)
-		// if err != nil {
-		// 	return fmt.Errorf("%s %s state differs from expected : %s", name, partitionAccessResource.Primary.Attributes["name"], err.Error())
-		// }
+		fmt.Printf("NASHA partition : %+v\n", partitionResponse)
 
 		return nil
 	}
 }
 
-func (w *TestAccDedicatedNASHAWrapper) TestDestroy(state *terraform.State) error {
-	leftovers := false
-
-	for _, resource := range state.RootModule().Resources {
-		if resource.Type == "ovh_dedicated_nasha_partition_access" {
-			config := testAccProvider.Meta().(*Config)
-			service := os.Getenv("OVH_NASHA_SERVICE")
-			partition := w.ExpectedPartitionAccess.PartitionName
-			ip := url.PathEscape(resource.Primary.Attributes["ip"])
-			endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s/access/%s", service, partition, ip)
-			err := config.OVHClient.Get(endpoint, nil)
-			if err == nil {
-				leftovers = true
-			}
+func testAccNashaPartitionSnapshotCheck(n string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		partitionResource, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Resource not found: %s", n)
 		}
-	}
 
-	for _, resource := range state.RootModule().Resources {
-		if resource.Type == "ovh_dedicated_nasha_partition" {
-			config := testAccProvider.Meta().(*Config)
-			service := os.Getenv("OVH_NASHA_SERVICE")
-			partition := w.ExpectedPartition.Name
-			// ip := url.PathEscape(resource.Primary.Attributes["ip"])
-			endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s", service, partition)
-			err := config.OVHClient.Get(endpoint, nil)
-			if err == nil {
-				leftovers = true
-			}
+		serviceName := partitionResource.Primary.Attributes["service_name"]
+		partitionName := partitionResource.Primary.Attributes["partition_name"]
+		snapshotType := partitionResource.Primary.Attributes["type"]
+
+		config := testAccProvider.Meta().(*Config)
+		endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s/snapshot/%s", serviceName, partitionName, snapshotType)
+		partitionSnapshotResponse := &DedicatedNASHAPartitionSnapshot{}
+		err := config.OVHClient.Get(endpoint, partitionSnapshotResponse)
+		if err != nil {
+			return fmt.Errorf("calling GET %s :\n\t %s", endpoint, err.Error())
 		}
+
+		fmt.Printf("NASHA partition snapshot : %+v\n", partitionSnapshotResponse)
+
+		return nil
 	}
-	if leftovers {
-		return fmt.Errorf("ovh_dedicated_nasha_partition still exists")
-	}
-	return nil
 }
 
-func newTestAccDedicatedNASHAWrapper() *TestAccDedicatedNASHAWrapper {
-	w := &TestAccDedicatedNASHAWrapper{}
-	hash := md5.Sum([]byte(time.Now().String()))
-	w.RandomName = "testacc_" + hex.EncodeToString(hash[:])
-	w.ExpectedPartition = &TestAccDedicatedNASHAPartition{ServiceName: os.Getenv("OVH_NASHA_SERVICE"), Name: w.RandomName}
-	w.ExpectedPartitionAccess = &TestAccDedicatedNASHAPartitionAccess{ServiceName: os.Getenv("OVH_NASHA_SERVICE"), PartitionName: w.RandomName}
-	return w
+func testAccNashaPartitionAccessCheck(n string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		partitionResource, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Resource not found: %s", n)
+		}
+
+		serviceName := partitionResource.Primary.Attributes["service_name"]
+		partitionName := partitionResource.Primary.Attributes["partition_name"]
+		accessIp := partitionResource.Primary.Attributes["ip"]
+
+		config := testAccProvider.Meta().(*Config)
+		endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s/access/%s", serviceName, partitionName, url.PathEscape(accessIp))
+		partitionAccessResponse := &DedicatedNASHAPartitionAccess{}
+		err := config.OVHClient.Get(endpoint, partitionAccessResponse)
+		if err != nil {
+			return fmt.Errorf("calling GET %s :\n\t %s", endpoint, err.Error())
+		}
+
+		fmt.Printf("NASHA partition access : %+v\n", partitionAccessResponse)
+
+		return nil
+	}
 }
 
-func TestAccDedicatedNASHA(t *testing.T) {
-	for _, plan := range TestAccDedicatedNASHAPlan {
-		w := newTestAccDedicatedNASHAWrapper()
-		var steps []resource.TestStep
-		for _, tcase := range plan {
-			steps = append(steps, w.TestStep(tcase))
+func testAccNashaPartitionDestroy(n string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		nashaPartitionAccessResource, ok := state.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Resource not found: %s", n)
 		}
-		resource.Test(t, resource.TestCase{
-			Providers:    testAccProviders,
-			CheckDestroy: w.TestDestroy,
-			Steps:        steps,
-		})
+
+		serviceName := nashaPartitionAccessResource.Primary.Attributes["service_name"]
+		parititionName := nashaPartitionAccessResource.Primary.Attributes["name"]
+
+		config := testAccProvider.Meta().(*Config)
+		endpoint := fmt.Sprintf("/dedicated/nasha/%s/partition/%s", serviceName, parititionName)
+
+		err := config.OVHClient.Get(endpoint, nil)
+		if err == nil {
+			return fmt.Errorf("NASHA Partition (%s) still exists", parititionName)
+		}
+
+		return nil
 	}
 }
