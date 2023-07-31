112320bc7e3970397e760b5281ade384d6381385
bbellion <bastien.bellion@ovhcloud.com>
2023-07-31 16:40:04 +0200

    Add Database kafka schema registry

---
+++ ovh/data_cloud_project_database_kafka_schemaregistryacls_test.go
@@ -0,0 +1,87 @@
+package ovh
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+const testAccCloudProjectDatabaseKafkaSchemaregistryaclsDatasourceConfig_Basic = `
+resource "ovh_cloud_project_database" "db" {
+	service_name = "%s"
+	description  = "%s"
+	engine       = "kafka"
+	version      = "%s"
+	plan         = "business"
+	nodes {
+		region     = "%s"
+	}
+	nodes {
+		region     = "%s"
+	}
+	nodes {
+		region     = "%s"
+	}
+	flavor = "%s"
+}
+
+resource "ovh_cloud_project_database_kafka_schemaregistryacl" "schemaRegistryAcl" {
+	service_name = ovh_cloud_project_database.db.service_name
+	cluster_id   = ovh_cloud_project_database.db.id
+	permission	 = "%s"
+	resource 	 = "%s"
+	username 	 = "%s"
+}
+
+data "ovh_cloud_project_database_kafka_schemaregistryacls" "schemaRegistryAcls" {
+  service_name = ovh_cloud_project_database_kafka_acl.acl.service_name
+  cluster_id   = ovh_cloud_project_database_kafka_acl.acl.cluster_id
+}
+`
+
+func TestAccCloudProjectDatabaseKafkaSchemaregistryaclsDataSource_basic(t *testing.T) {
+	serviceName := os.Getenv("OVH_CLOUD_PROJECT_SERVICE_TEST")
+	version := os.Getenv("OVH_CLOUD_PROJECT_DATABASE_KAFKA_VERSION_TEST")
+	if version == "" {
+		version = os.Getenv("OVH_CLOUD_PROJECT_DATABASE_VERSION_TEST")
+	}
+	region := os.Getenv("OVH_CLOUD_PROJECT_DATABASE_REGION_TEST")
+	flavor := os.Getenv("OVH_CLOUD_PROJECT_DATABASE_FLAVOR_TEST")
+	description := acctest.RandomWithPrefix(test_prefix)
+	permission := "schema_registry_read"
+	aclResource := "Subject:myResource"
+	username := "johnDoe"
+
+	config := fmt.Sprintf(
+		testAccCloudProjectDatabaseKafkaAclsDatasourceConfig_Basic,
+		serviceName,
+		description,
+		version,
+		region,
+		region,
+		region,
+		flavor,
+		permission,
+		aclResource,
+		username,
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheckCloudDatabaseNoEngine(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(
+						"data.ovh_cloud_project_database_kafka_schemaregistryacls.schemaRegistryAcls",
+						"acl_ids.#",
+					),
+				),
+			},
+		},
+	})
+}
