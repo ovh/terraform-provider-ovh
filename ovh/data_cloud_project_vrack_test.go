8a5ccd0e543a5dda41ad176d4a0a1b4f26a6e3aa
Arthur Amstutz <arthur.amstutz@corp.ovh.com>
2024-03-04 16:55:30 +0000

    fix: TestAccCloudProjectVrackDataSource_basic: Attach a vRack before test and detach it afterwards

--- ovh/data_cloud_project_vrack_test.go
+++ ovh/data_cloud_project_vrack_test.go
@@ -22,7 +22,13 @@ func TestAccCloudProjectVrackDataSource_basic(t *testing.T) {
 }
 
 var testAccCloudProjectVrackDatasourceConfig = fmt.Sprintf(`
+resource "ovh_vrack_cloudproject" "attach" {
+	service_name = "%s"
+	project_id   = "%s"
+}
+
 data "ovh_cloud_project_vrack" "vrack" {
   service_name = "%s"
+  depends_on = [ovh_vrack_cloudproject.attach]
 }
-`, os.Getenv("OVH_CLOUD_PROJECT_SERVICE_TEST"))
+`, os.Getenv("OVH_VRACK_SERVICE_TEST"), os.Getenv("OVH_CLOUD_PROJECT_SERVICE_TEST"), os.Getenv("OVH_CLOUD_PROJECT_SERVICE_TEST"))
