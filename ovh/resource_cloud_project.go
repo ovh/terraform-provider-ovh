aa0edf506bbbdf958105279fabdfb9b5064bf949
Arthur Amstutz <arthur.amstutz@corp.ovh.com>
2024-07-08 13:14:03 +0000

    fix: Retrieve service information when importing/reading ovh_cloud_project

--- ovh/resource_cloud_project.go
+++ ovh/resource_cloud_project.go
@@ -150,6 +150,20 @@ func resourceCloudProjectRead(d *schema.ResourceData, meta interface{}) error {
 		d.Set(k, v)
 	}
 
+	// Retrieve order information
+	serviceObj, err := serviceFromServiceName(config.OVHClient, "cloud/project", serviceName)
+	if err != nil {
+		return fmt.Errorf("failed to retrieve cloud project details: %w", err)
+	}
+	d.Set("plan", serviceObj.ToSDKv2PlanValue())
+
+	// Retrieve subsidiary information
+	var me MeResponse
+	if err := config.OVHClient.Get("/me", &me); err != nil {
+		return fmt.Errorf("error retrieving account information: %w", err)
+	}
+	d.Set("ovh_subsidiary", me.OvhSubsidiary)
+
 	return nil
 }
 
