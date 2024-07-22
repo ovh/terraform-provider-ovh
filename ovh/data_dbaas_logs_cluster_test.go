5a5f025bdc35dd5a82dd75910206375bfa4657a2
Arthur Amstutz <arthur.amstutz@corp.ovh.com>
2024-07-22 07:19:49 +0000

    fix: correctly set cluster_id field in datasource ovh_dbaas_logs_cluster

--- ovh/data_dbaas_logs_cluster_test.go
+++ ovh/data_dbaas_logs_cluster_test.go
@@ -8,19 +8,15 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-const testAccDataSourceDbaasLogsCluster = `
-data "ovh_dbaas_logs_cluster" "ldp" {
-  service_name = "%s"
-  cluster_id   = "%s"
-}
-`
-
 func TestAccDataSourceDbaasLogsCluster(t *testing.T) {
 	serviceName := os.Getenv("OVH_DBAAS_LOGS_SERVICE_TEST")
 	clusterId := os.Getenv("OVH_DBAAS_LOGS_CLUSTER_ID")
 
-	config := fmt.Sprintf(
-		testAccDataSourceDbaasLogsCluster,
+	config := fmt.Sprintf(`
+		data "ovh_dbaas_logs_cluster" "ldp" {
+			service_name = "%s"
+			cluster_id   = "%s"
+		}`,
 		serviceName,
 		clusterId,
 	)
@@ -37,6 +33,46 @@ func TestAccDataSourceDbaasLogsCluster(t *testing.T) {
 						"data.ovh_dbaas_logs_cluster.ldp",
 						"service_name",
 						serviceName,
+					),
+					resource.TestCheckResourceAttr(
+						"data.ovh_dbaas_logs_cluster.ldp",
+						"cluster_id",
+						clusterId,
+					),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceDbaasLogsClusterDefault(t *testing.T) {
+	serviceName := os.Getenv("OVH_DBAAS_LOGS_SERVICE_TEST")
+	clusterId := os.Getenv("OVH_DBAAS_LOGS_CLUSTER_ID")
+
+	config := fmt.Sprintf(`
+		data "ovh_dbaas_logs_cluster" "ldp" {
+			service_name = "%s"
+		}`,
+		serviceName,
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheckDbaasLogsCluster(t) },
+
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.ovh_dbaas_logs_cluster.ldp",
+						"service_name",
+						serviceName,
+					),
+					resource.TestCheckResourceAttr(
+						"data.ovh_dbaas_logs_cluster.ldp",
+						"cluster_id",
+						clusterId,
 					),
 				),
 			},
