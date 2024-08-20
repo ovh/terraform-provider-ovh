96b63db73a8bbcfb678f672624f4e5da86eec06f
Arthur Amstutz <arthur.amstutz@corp.ovh.com>
2024-08-20 14:00:55 +0000

    fix: ovh_cloud_project_user_s3_policy: Read policy field from API

--- ovh/testing.go
+++ ovh/testing.go
@@ -1,5 +1,34 @@
 package ovh
 
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+)
+
 const (
 	test_prefix = "testacc-terraform"
 )
+
+// Custom type used to check that a plan is empty in acceptance tests
+var _ plancheck.PlanCheck = expectEmptyPlan{}
+
+type expectEmptyPlan struct{}
+
+func (e expectEmptyPlan) CheckPlan(ctx context.Context, req plancheck.CheckPlanRequest, resp *plancheck.CheckPlanResponse) {
+	var result error
+
+	for _, rc := range req.Plan.ResourceChanges {
+		if !rc.Change.Actions.NoOp() {
+			result = errors.Join(result, fmt.Errorf("expected empty plan, but %s has planned action(s): %v", rc.Address, rc.Change.Actions))
+		}
+	}
+
+	resp.Error = result
+}
+
+func ExpectEmptyPlan() plancheck.PlanCheck {
+	return expectEmptyPlan{}
+}
