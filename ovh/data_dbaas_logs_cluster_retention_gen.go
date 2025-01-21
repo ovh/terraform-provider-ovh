4220436b94a9fde6fa7d577ba89f8abdc2799ba4
Arthur Amstutz <arthur.amstutz@corp.ovh.com>
2025-01-21 09:30:57 +0000

    feat: Add retention_type to datasource ovh_dbaas_logs_cluster_retention

--- ovh/data_dbaas_logs_cluster_retention_gen.go
+++ ovh/data_dbaas_logs_cluster_retention_gen.go
@@ -5,7 +5,10 @@ package ovh
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	ovhtypes "github.com/ovh/terraform-provider-ovh/ovh/types"
 )
 
@@ -18,9 +21,12 @@ func DbaasLogsClusterRetentionDataSourceSchema(ctx context.Context) schema.Schem
 			MarkdownDescription: "Cluster ID",
 		},
 		"duration": schema.StringAttribute{
-			CustomType:          ovhtypes.TfStringType{},
-			Computed:            true,
-			Optional:            true,
+			CustomType: ovhtypes.TfStringType{},
+			Computed:   true,
+			Optional:   true,
+			Validators: []validator.String{
+				stringvalidator.ConflictsWith(path.MatchRoot("retention_id")),
+			},
 			Description:         "Indexed duration expressed in ISO-8601 format",
 			MarkdownDescription: "Indexed duration expressed in ISO-8601 format",
 		},
@@ -31,11 +37,30 @@ func DbaasLogsClusterRetentionDataSourceSchema(ctx context.Context) schema.Schem
 			MarkdownDescription: "Indicates if a new stream can use it",
 		},
 		"retention_id": schema.StringAttribute{
+			CustomType: ovhtypes.TfStringType{},
+			Computed:   true,
+			Optional:   true,
+			Validators: []validator.String{
+				stringvalidator.ConflictsWith(path.MatchRoot("duration")),
+				stringvalidator.ConflictsWith(path.MatchRoot("retention_type")),
+			},
+			Description:         "Retention ID",
+			MarkdownDescription: "Retention ID",
+		},
+		"retention_type": schema.StringAttribute{
 			CustomType:          ovhtypes.TfStringType{},
 			Computed:            true,
 			Optional:            true,
-			Description:         "Retention ID",
-			MarkdownDescription: "Retention ID",
+			Description:         "Retention type",
+			MarkdownDescription: "Retention type",
+			Validators: []validator.String{
+				stringvalidator.OneOf(
+					"LOGS_COLD_STORAGE",
+					"LOGS_INDEXING",
+					"METRICS_TENANT",
+				),
+				stringvalidator.ConflictsWith(path.MatchRoot("retention_id")),
+			},
 		},
 		"service_name": schema.StringAttribute{
 			CustomType:          ovhtypes.TfStringType{},
@@ -51,11 +76,12 @@ func DbaasLogsClusterRetentionDataSourceSchema(ctx context.Context) schema.Schem
 }
 
 type DbaasLogsClusterRetentionModel struct {
-	ClusterId   ovhtypes.TfStringValue `tfsdk:"cluster_id" json:"clusterId"`
-	Duration    ovhtypes.TfStringValue `tfsdk:"duration" json:"duration"`
-	IsSupported ovhtypes.TfBoolValue   `tfsdk:"is_supported" json:"isSupported"`
-	RetentionId ovhtypes.TfStringValue `tfsdk:"retention_id" json:"retentionId"`
-	ServiceName ovhtypes.TfStringValue `tfsdk:"service_name" json:"serviceName"`
+	ClusterId     ovhtypes.TfStringValue `tfsdk:"cluster_id" json:"clusterId"`
+	Duration      ovhtypes.TfStringValue `tfsdk:"duration" json:"duration"`
+	IsSupported   ovhtypes.TfBoolValue   `tfsdk:"is_supported" json:"isSupported"`
+	RetentionId   ovhtypes.TfStringValue `tfsdk:"retention_id" json:"retentionId"`
+	RetentionType ovhtypes.TfStringValue `tfsdk:"retention_type" json:"retentionType"`
+	ServiceName   ovhtypes.TfStringValue `tfsdk:"service_name" json:"serviceName"`
 }
 
 func (v *DbaasLogsClusterRetentionModel) MergeWith(other *DbaasLogsClusterRetentionModel) {
@@ -73,6 +99,10 @@ func (v *DbaasLogsClusterRetentionModel) MergeWith(other *DbaasLogsClusterRetent
 
 	if (v.RetentionId.IsUnknown() || v.RetentionId.IsNull()) && !other.RetentionId.IsUnknown() {
 		v.RetentionId = other.RetentionId
+	}
+
+	if (v.RetentionType.IsUnknown() || v.RetentionType.IsNull()) && !other.RetentionType.IsUnknown() {
+		v.RetentionType = other.RetentionType
 	}
 
 	if (v.ServiceName.IsUnknown() || v.ServiceName.IsNull()) && !other.ServiceName.IsUnknown() {
