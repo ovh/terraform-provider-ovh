909a62ce0ae2783d3f8b728b39f6eb3782b09a40
Arthur Amstutz <arthur.amstutz@corp.ovh.com>
2025-02-27 09:50:32 +0000

    feat: Add s3 storage datasources

--- ovh/provider_new.go
+++ ovh/provider_new.go
@@ -199,6 +199,8 @@ func (p *OvhProvider) DataSources(_ context.Context) []func() datasource.DataSou
 		NewCloudProjectNetworkPrivateDataSource,
 		NewCloudProjectNetworkPrivatesDataSource,
 		NewCloudProjectNetworkPrivateSubnetsDataSource,
+		NewCloudProjectStorageDataSource,
+		NewCloudProjectStoragesDataSource,
 		NewCloudProjectVolumesDataSource,
 		NewCloudProjectVolumeDataSource,
 		NewCloudProjectDataSource,
