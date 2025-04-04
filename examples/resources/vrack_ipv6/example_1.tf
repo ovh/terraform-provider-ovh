92ec3b1557308526fcdfa54db578a4b2b59e44b4
Romain Chereau <romain.chereau@ovhcloud.com>
2025-04-04 14:37:20 +0000

    feat(vrack): complete resource ovh_vrack_ipv6 and add resource ovh_vrack_ipv6_routed_subrange

--- examples/resources/vrack_ipv6/example_1.tf
+++ examples/resources/vrack_ipv6/example_1.tf
@@ -1,4 +1,12 @@
-resource "ovh_vrack_ipv6" "vrack_block" {
+resource "ovh_vrack_ipv6" "vrack_block_v6" {
+  service_name = "<vRack service name>"
+  block        = "<ipv6 block>"
+  bridged_subrange {
+    slaac = "<enabled|disabled>"
+  }
+}
+
+resource "ovh_vrack_ipv6" "vrack_block_v6" {
   service_name = "<vRack service name>"
   block        = "<ipv6 block>"
 }
