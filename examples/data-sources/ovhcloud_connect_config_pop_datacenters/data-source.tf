10c18ce8a1919dfd9277c19f756dcb2b54cf936e
Johanne Pereau <johanne.pereau@corp.ovh.com>
2025-04-09 15:59:25 +0200

    feat: add Datasources for configuration of Ovhcloud Connect (OCC) product

---
+++ examples/data-sources/ovhcloud_connect_config_pop_datacenters/data-source.tf
@@ -0,0 +1,4 @@
+data "ovh_ovhcloud_connect_config_pop_datacenters" "datacenter_cfgs" {
+  service_name = "XXX"
+  config_pop_id = "YYY"
+}
---
+++ examples/data-sources/ovhcloud_connect_config_pops/data-source.tf
@@ -0,0 +1,3 @@
+data "ovh_ovhcloud_connect_config_pops" "pop_cfgs" {
+  service_name = "XXX"
+}
