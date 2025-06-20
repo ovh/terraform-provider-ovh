c0c84916d3c8d3c06aa1146b44936934b32636d9
Jérémy Dubus <jeremy.dubus@ovhcloud.com>
2025-06-20 09:05:54 +0000

    feat(ip_reverse): add readiness_timeout_duration attribute

--- examples/resources/ip_reverse/example_1.tf
+++ examples/resources/ip_reverse/example_1.tf
@@ -1,6 +1,7 @@
 # Set the reverse of an IP
 resource "ovh_ip_reverse" "test" {
-  ip = "192.0.2.0/24"
-  ip_reverse = "192.0.2.1"
-  reverse = "example.com"
+  readiness_timeout_duration = "1m"
+  ip                         = "192.0.2.0/24"
+  ip_reverse                 = "192.0.2.1"
+  reverse                    = "example.com"
 }
