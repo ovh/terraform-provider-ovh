d2c972eef1e339045fe510f41e7fc2a74747159f
Arthur Amstutz <arthur.amstutz@corp.ovh.com>
2025-08-25 12:07:47 +0000

    fix acceptance tests

--- ovh/resource_ip_firewall_test.go
+++ ovh/resource_ip_firewall_test.go
@@ -2,11 +2,42 @@ package ovh
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
+
+func init() {
+	resource.AddTestSweepers("ovh_ip_firewall", &resource.Sweeper{
+		Name: "ovh_ip_firewall",
+		F:    testSweepIPFirewall,
+	})
+}
+
+func testSweepIPFirewall(region string) error {
+	client, err := sharedClientForRegion(region)
+	if err != nil {
+		return fmt.Errorf("error getting client: %s", err)
+	}
+
+	ip := os.Getenv("OVH_IP_TEST")
+	endpoint := fmt.Sprintf("/ip/%s/firewall", url.PathEscape(ip))
+
+	var ips []string
+	if err := client.Get(endpoint, &ips); err != nil {
+		return fmt.Errorf("Error calling %s:\n\t %q", endpoint, err)
+	}
+
+	for _, ipOnFirewall := range ips {
+		if err := client.Delete(fmt.Sprintf("/ip/%s/firewall/%s", url.PathEscape(ip), url.PathEscape(ipOnFirewall)), nil); err != nil {
+			return fmt.Errorf("Error deleting ip firewall %s on ip %s:\n\t %q", ipOnFirewall, ip, err)
+		}
+	}
+
+	return nil
+}
 
 const testAccIPFirewallConfig = `
 resource "ovh_ip_firewall" "firewall" {
