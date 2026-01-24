0742635014cdc5a718c982a459a346803cfd0ad6
Pascal T. <pascal@toepke.dev>
2026-01-24 03:54:28 +0100

    feat: merge generated schema into resource_cloud_project_storage_replication_job.go
    Signed-off-by: Pascal T. <pascal@toepke.dev>

--- ovh/resource_cloud_project_storage_replication_job.go
+++ ovh/resource_cloud_project_storage_replication_job.go
@@ -6,6 +6,9 @@ import (
 	"net/url"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	ovhtypes "github.com/ovh/terraform-provider-ovh/v2/ovh/types"
 )
 
@@ -92,4 +95,61 @@ func (r *cloudProjectStorageReplicationJobResource) Update(ctx context.Context, 
 func (r *cloudProjectStorageReplicationJobResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	// No-op: Replication jobs cannot be cancelled or deleted once triggered
 	// Simply remove from state
+}
+
+func CloudProjectStorageReplicationJobResourceSchema(ctx context.Context) schema.Schema {
+	return schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				CustomType:  ovhtypes.TfStringType{},
+				Computed:    true,
+				Description: "Unique identifier for the replication job",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"service_name": schema.StringAttribute{
+				CustomType:          ovhtypes.TfStringType{},
+				Required:            true,
+				Description:         "The ID of the public cloud project",
+				MarkdownDescription: "The ID of the public cloud project. If omitted, the `OVH_CLOUD_PROJECT_SERVICE` environment variable is used.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"region_name": schema.StringAttribute{
+				CustomType:          ovhtypes.TfStringType{},
+				Required:            true,
+				Description:         "Region name of the storage container",
+				MarkdownDescription: "Region name of the storage container",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+			"container_name": schema.StringAttribute{
+				CustomType:          ovhtypes.TfStringType{},
+				Required:            true,
+				Description:         "Name of the storage container",
+				MarkdownDescription: "Name of the storage container",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
+			},
+		},
+		Description:         "Trigger a one-time replication job for a storage container.",
+		MarkdownDescription: "Trigger a one-time replication job for a storage container. This is useful for catch-up replication of existing objects when setting up new replication rules.",
+	}
+}
+
+// CloudProjectStorageReplicationJobModel describes the resource data model.
+type CloudProjectStorageReplicationJobModel struct {
+	ID            ovhtypes.TfStringValue `tfsdk:"id"`
+	ServiceName   ovhtypes.TfStringValue `tfsdk:"service_name"`
+	RegionName    ovhtypes.TfStringValue `tfsdk:"region_name"`
+	ContainerName ovhtypes.TfStringValue `tfsdk:"container_name"`
+}
+
+// CloudProjectStorageReplicationJobResponse describes the API response.
+type CloudProjectStorageReplicationJobResponse struct {
+	ID string `json:"id"`
 }
